dc27d1b3f3fa30166c5db2ef98f2d5e1c6f18008
Jose Miguel Perez <69158653+miguelperezlq@users.noreply.github.com>
2025-05-01 21:53:36 -0300

    subiendo archivos
    subiendo porfolio

---
+++ script.js
@@ -0,0 +1,43 @@
+// navbar toggle
+const icon = document.querySelector('i.navbar-toggler');
+icon.addEventListener('click', function()
+{
+    this.classList.contains('collapsed')? this.classList.replace('bx-x','bx-menu'):this.classList.replace('bx-menu','bx-x');
+});
+
+// active menu
+const allMenu = document.querySelectorAll('nav .navbar-nav a');
+const sections = document.querySelectorAll('section:not(#contact)');
+
+window.addEventListener('scroll', function(){
+    let current = '';
+    sections.forEach(function(sect){
+        if (this.pageYOffset + 50 > sect.offsetTop){
+            current = sect.getAttribute('id');
+        }
+    });
+
+    allMenu.forEach(function(nav){
+        nav.classList.remove('active');
+        if (nav.classList.contains(current)){
+            nav.classList.add('active');
+        }
+    });
+});
+
+const loader =$('.loading');
+$(window).on('load',function(){
+    loader.fadeOut();
+});
+
+
+let options = {
+    strings:['Diseñador Web', 'Desarrollador Web'],
+    typeSpeed:70,
+    backSpeed:50,
+    smartBackspace:false,
+    backDelay:2000,
+    loop:true,
+}
+
+let typed = new Typed('.typed', options);
